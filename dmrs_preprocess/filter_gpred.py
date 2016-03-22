aef5d5caf7f2b1499391217c5383f0397d40a895
Matic Horvat <Matic Horvat>
2016-03-22 19:57:11 +0000

    Added ltop handling to gpred filtering

--- dmrs_preprocess/filter_gpred.py
+++ dmrs_preprocess/filter_gpred.py
@@ -35,7 +35,7 @@ def curb_gpred_spans(dmrs_xml, max_tokens=3):
 
 
 def filter_gpred(dmrs_xml, gpred_filter, handle_ltop=False, allow_disconnected_dmrs=False):
-    '''
+    """
     Remove general predicate nodes on the filter list from the DMRS.
     :param dmrs_xml: Input DMRS XML
     :param gpred_filters: A set of general predicates to filter
@@ -43,7 +43,7 @@ def filter_gpred(dmrs_xml, gpred_filter, handle_ltop=False, allow_disconnected_d
     :param allow_disconnected_dmrs: Remove gpred nodes even if their removal would result in a disconnected DMRS.
      If DMRS was already disconnected, gpred nodes are removed regardless.
     :return: Modified DMRS XML
-    '''
+    """
 
     dmrs_graph, filterable_nodes = parse_dmrs_xml(dmrs_xml, gpred_filter)
 
@@ -59,6 +59,12 @@ def filter_gpred(dmrs_xml, gpred_filter, handle_ltop=False, allow_disconnected_d
     else:
         filtered_nodes = filterable_nodes
 
+    if handle_ltop and dmrs_xml.attrib.get('ltop') in filtered_nodes:
+        new_ltop = choose_new_ltop(dmrs_xml, dmrs_graph, filtered_nodes)
+
+        if new_ltop is not None:
+            dmrs_xml.attrib['ltop'] = new_ltop
+
     # Remove filtered nodes and their links from the DMRS
     remove_filtered_nodes(dmrs_xml, filtered_nodes)
 
@@ -70,11 +76,12 @@ def parse_dmrs_xml(dmrs_xml, gpred_filter):
     Parse DMRS XML to node and edge sets and identify filterable_nodes.
     :param dmrs_xml: DMRS XML object
     :param gpred_filter: A set of general predicates to filter
-    :return: ((nodes, edges), filterable_nodes) tuples
+    :return: ((nodes, undirected_edges, directed_edges), filterable_nodes) tuples
     """
 
     nodes = set()
-    links = defaultdict(set)
+    undirected_edges = defaultdict(set)
+    directed_edges = defaultdict(set)
     filterable_nodes = set()
 
     for entity in dmrs_xml:
@@ -90,10 +97,11 @@ def parse_dmrs_xml(dmrs_xml, gpred_filter):
             link_from = entity.attrib['from']
             link_to = entity.attrib['to']
 
-            links[link_from].add(link_to)
-            links[link_to].add(link_from)
+            undirected_edges[link_from].add(link_to)
+            undirected_edges[link_to].add(link_from)
+            directed_edges[link_from].add(link_to)
 
-    return (nodes, links), filterable_nodes
+    return (nodes, undirected_edges, directed_edges), filterable_nodes
 
 
 def gpred_node(node_xml):
@@ -115,7 +123,7 @@ def gpred_node(node_xml):
 def is_connected(dmrs_graph, removed_nodeids=frozenset(), ignored_nodeids=frozenset()):
     """
     Determine if a DMRS graph is connected.
-    :param dmrs_graph: Tuple of sets (nodes, edges)
+    :param dmrs_graph: Tuple of sets (nodes, undirected_edges, directed_edges)
     :param removed_nodeids: Set of node ids that should be considered as already removed.
      This is to prevent the need for excessive copying of DMRS graphs for hypothetical node removals.
     :param ignored_nodeids: Set of node ids that should not be considered as disconnected if found as such.
@@ -129,13 +137,14 @@ def is_connected(dmrs_graph, removed_nodeids=frozenset(), ignored_nodeids=frozen
 def compute_disconnected_nodeids(dmrs_graph, removed_nodeids=frozenset()):
     """
     Search for disconnected nodes.
-    :param dmrs_graph: Tuple of sets (nodes, edges)
+    :param dmrs_graph: Tuple of sets (nodes, undirected_edges, directed_edges)
     :param removed_nodeids: Set of node ids that should be considered as already removed.
      This is to prevent the need for excessive copying of DMRS graphs for hypothetical node removals.
     :return: Set of disconnected node ids
     """
 
-    nodes, edges = dmrs_graph
+    # Operate on undirected edges
+    nodes, edges, _ = dmrs_graph
 
     # Initialize the set of node that have not been visited yet
     unvisited_nodeids = set(nodes) - removed_nodeids
@@ -161,11 +170,66 @@ def get_neighbours(node, edges):
     return edges[node]
 
 
+def choose_new_ltop(dmrs_xml, dmrs_graph, filtered_nodes):
+    """
+    Choose the new LTOP because old LTOP is being filtered out.
+    :param dmrs_xml: DMRS XML object
+    :param dmrs_graph: Tuple of sets (nodes, undirected_edges, directed_edges)
+    :param filtered_nodes: List of nodes to filter
+    """
+
+    ltop = dmrs_xml.attrib.get('ltop')
+    index = dmrs_xml.attrib.get('index')
+
+    # Operate on directed edges
+    nodes, undirected_edges, edges = dmrs_graph
+
+    # Iteratively search for new ltop
+    while True:
+
+        # Try to select it from previous ltop children
+        ltop_children = list(get_neighbours(ltop, edges))
+
+        # If only one child exists and it hasn't been filtered, choose it as new ltop
+        if len(ltop_children) == 1 and ltop_children[0] not in filtered_nodes:
+            return ltop_children[0]
+
+        # Or if multiple children exist and one of them equals to index, choose it as new ltop
+        elif index in ltop_children:
+            return index
+
+        # Try to select it from previous ltop parents
+        ltop_parents = list(get_neighbours(ltop, undirected_edges) - get_neighbours(ltop, edges))
+
+        # If only one parent exists and it hasn't been filtered, choose it as new ltop
+        if len(ltop_parents) == 1 and ltop_parents[0] not in filtered_nodes:
+            return ltop_parents[0]
+
+        # If only one child exists and it has been filtered, choose it as temporary ltop and iterate
+        if len(ltop_children) == 1:
+            ltop = ltop_children[0]
+            continue
+
+        # If only one parent exists and it has been filtered, choose it as temporary ltop and iterate
+        if len(ltop_parents) == 1:
+            ltop = ltop_parents[0]
+            continue
+
+        break
+
+    # As a last resort, choose a random unfiltered node, if it exists
+    if len(nodes - filtered_nodes) > 0:
+        return set(nodes - filtered_nodes).pop()
+
+    else:
+        return None
+
+
 def remove_filtered_nodes(dmrs_xml, filtered_nodes):
     """
     Remove nodes on filtered_nodes list and associated links from dmrs_xml.
-    :param dmrs_xml:
-    :param filtered_nodes:
+    :param dmrs_xml: DMRS XML object
+    :param filtered_nodes: List of nodes to filter
     """
 
     entities_to_remove = []
