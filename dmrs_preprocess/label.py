488ccb1040d9bb9a2a5b7fe544f82e28b87530a2
Matic Horvat <Matic Horvat>
2016-10-04 17:26:13 +0100

    Initial work on improving labeling

--- dmrs_preprocess/label.py
+++ dmrs_preprocess/label.py
@@ -1,45 +1,36 @@
 
 
 def create_label(dmrs_xml, carg_clean=False):
-    '''
+    """
     Create an identifying label attribute for each node and link,
     consisting of its arguments and properties.
     :param dmrs_xml: Input DMRS XML
     :return: Modified DMRS XML
-    '''
+    """
 
     for entity in dmrs_xml:
         if entity.tag == 'node':
 
-            node_attribs = dict()
-            # Remove quotes around CARG
-            if entity.attrib.get('carg') is not None:
-                if carg_clean:
-                    entity.attrib['carg'] = entity.attrib['carg'][1:-1]
+            node_attribs = collect_node_attribs(entity)
 
-                node_attribs['carg'] = entity.attrib['carg']
+            if node_attribs.get('gpred') is not None:
 
-            # Collect node attributes in a dictionary
-            for node_info in entity:
-                node_attribs.update(node_info.attrib)
+                # Remove quotes around CARG
+                if node_attribs.get('carg') is not None and carg_clean:
+                    clean_carg = node_attribs['carg'][1:-1]
+                    entity.attrib['carg'] = clean_carg
+                    node_attribs['carg'] = clean_carg
 
-                if node_info.tag == 'gpred':
-                    node_attribs[node_info.tag] = node_info.text
+                label = label_gpred(node_attribs)
 
-            # Construct the label attribute
-            tense = node_attribs.get('tense') if node_attribs.get('tense') is not None and node_attribs.get('tense').lower() != 'untensed' else None
-            sf = node_attribs.get('sf') if node_attribs.get('sf') != 'prop' else None
+            elif node_attribs.get('pos') == 'n':
+                label = label_noun(node_attribs)
 
-            attribs_of_interest = [node_attribs.get('carg'), node_attribs.get('gpred'),
-                                   node_attribs.get('lemma'), node_attribs.get('pos'),
-                                   node_attribs.get('sense'), node_attribs.get('pers'),
-                                   node_attribs.get('num'), tense,
-                                   node_attribs.get('gend'), sf]
+            elif node_attribs.get('pos') == 'v':
+                label = label_verb(node_attribs)
 
-            label = '_'.join([unicode(x) for x in attribs_of_interest if x is not None])
-
-            if node_attribs.get('gpred') is None:
-                label = '_' + label
+            else:
+                label = label_default(node_attribs)
 
             # Attach the label to node XML
             entity.attrib['label'] = label
@@ -53,3 +44,75 @@ def create_label(dmrs_xml, carg_clean=False):
             entity.attrib['label'] = '_'.join([x for x in [arg, post] if x is not None])
 
     return dmrs_xml
+
+
+def label_gpred(node_attribs):
+    label_list = [
+        node_attribs.get('carg'),
+        node_attribs.get('gpred'),
+        node_attribs.get('pers'),
+        node_attribs.get('num'),
+        node_attribs.get('gend')
+    ]
+
+    return '_'.join([unicode(x) for x in label_list if x is not None])
+
+
+def label_noun(node_attribs):
+    label_list = [
+        node_attribs.get('lemma'),
+        node_attribs.get('pos'),
+        node_attribs.get('sense'),
+        node_attribs.get('pers') if node_attribs.get('pers') is not None else '3',
+        node_attribs.get('num') if node_attribs.get('num') is not None else 'sg'
+    ]
+
+    return '_' + '_'.join([unicode(x) for x in label_list if x is not None])
+
+
+def label_verb(node_attribs):
+    label_list = [
+        node_attribs.get('lemma'),
+        node_attribs.get('pos'),
+        node_attribs.get('sense'),
+        node_attribs.get('tense'),
+        node_attribs.get('sf')
+    ]
+
+    return '_' + '_'.join([unicode(x) for x in label_list if x is not None])
+
+
+def label_default(node_attribs):
+    label_list = [
+        node_attribs.get('lemma'),
+        node_attribs.get('pos'),
+        node_attribs.get('sense')
+    ]
+
+    return '_' + '_'.join([unicode(x) for x in label_list if x is not None])
+
+
+def collect_node_attribs(node):
+    """
+    Collect node attributes in a dictionary
+    :param node: XML node
+    :return: Dictionary of node attributes
+    """
+
+    node_attribs = dict()
+    for node_info in node:
+        node_attribs.update(node_info.attrib)
+
+        if node_info.tag == 'gpred':
+            node_attribs[node_info.tag] = node_info.text
+
+    if node.attrib.get('carg') is not None:
+        node_attribs['carg'] = node.attrib['carg']
+
+    if node_attribs.get('tense') is not None and node_attribs.get('tense').lower() == 'untensed':
+        del node_attribs['tense']
+
+    if node_attribs.get('sf') == 'prop' or node_attribs.get('sf') == 'prop-or-ques':
+        del node_attribs['sf']
+
+    return node_attribs
